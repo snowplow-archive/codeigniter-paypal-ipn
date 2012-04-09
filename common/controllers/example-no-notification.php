d68b697caeed05ba855e973c4529f5b1940af220
Alex Dean <alexander.dean@keplarllp.com>
2012-04-09 21:32:44 +0100

    Some hopefully clarificatory changes so nobody gets caught out by the Smarty email requirement again

--- common/controllers/example-no-notification.php
+++ common/controllers/example-no-notification.php
@@ -1,7 +1,9 @@
 <?php
 // codeigniter/controllers/example.php
 
-/* This is an example controller showing how to use the PayPal_IPN library
+/* This is an example controller showing how to use the PayPal_IPN library. This is a
+ * simple example: it does not send any notification emails or similar, it simply
+ * logs the order to the database.
  * 
  * This file is copyright (c) 2011 Alexander Dean, alex@keplarllp.com
  * 
@@ -19,7 +21,7 @@
  * codeigniter-paypal-ipn. If not, see <http://www.gnu.org/licenses/>.
  */
 
-class Example extends Controller {
+class ExampleNoNotification extends Controller {
 
     // To handle the IPN post made by PayPal (uses the Paypal_Lib library).
     function ipn()
@@ -38,35 +40,14 @@ class Example extends Controller {
             // Now let's check what the payment status is and act accordingly
             if ($this->paypal_ipn->orderStatus == PayPal_IPN::PAID)
             {
-    		
-			   /*	
-			    *	UNCOMMENT BELOW IN ORDER TO USE EMAIL NOTIFICATIONS 
-				* ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-				*	You'll have to create your own email template using Smarty, Twig or similar for this to work, uncommenting will cause your Paypal IPN
-				*	responses to fail without your knowledge.
-				*/
-				
-				/*
-                // Configure to send HTML emails.
-                $this->load->library('email');
-                $mail_config['mailtype'] = 'html';
-                $this->email->initialize($mail_config);
-
-                // Prepare the variables to populate the email template:
-                $data = $this->paypal_ipn->order;
-                $data['items'] = $this->paypal_ipn->orderItems;
-
-                // Now construct the email
-                $emailBody = $this->smarty->view('confirmation_email.tpl', $data, TRUE); // You'll have to create your own email template using Smarty, Twig or similar
-
-                // Finish configuring email contents and send.
-                $this->email->to($data['payer_email'], ($data['first_name'] . ' ' . $data['last_name']));
-                $this->email->bcc('sales@CHANGEME.com');
-                $this->email->from('support@CHANGEME.com', 'CHANGEME');
-                $this->email->subject('Order confirmation');
-                $this->email->message($emailBody);
-                $this->email->send();
-				*/
+                /* HEALTH WARNING:
+                 *
+                 * Please note that this PAID block does nothing. In other words, this controller will not respond to a successful order
+                 * with any notification such as email or similar. You will have to identify paid orders by checking your database.
+                 *
+                 * If you want to send email notifications on successful receipt of an order, please see the alternative, Smarty template-
+                 * based example controller: example-smarty-email-notification.php
+                 */
             }
         }
         else // Just redirect to the root URL
